cbdaa979faddd6c4a0c458fab120affe6d2049ff
CiaraSookarry <cjsookarry@gmail.com>
2021-11-20 22:15:49 +0000

    Complete training and testing sets for all letters

--- split_sets.py
+++ split_sets.py
@@ -7,65 +7,73 @@
 # Author: Ciara Sookarry
 # Date: 7 Nov 2021
 
+import fnmatch
 import glob
 import numpy
+import random
 
 from PIL import Image
-from sklearn.model_selection import train_test_split  # Splits data
+from sklearn.model_selection import train_test_split    # Splits data
+from termcolor import colored                           # Prints coloured text to terminal
+
+#####################################
+# Create input list and label list
+#####################################
+X_list = list()
+y_list = list()
 
 ##########################
 # Import images as lists
 ##########################
-a_list = glob.glob("/home/ciara/Documents/ISLDataset/ISL_50k/Frames_A/Person6*.jpg")
-b_list = glob.glob("/home/ciara/Documents/ISLDataset/ISL_50k/Frames_B/Person6*.jpg")
+img_list = glob.glob("/home/ciara/Documents/ISLDataset/**/Frames_*/Person*", recursive=True) # will return 48345 image locations
 
 #########################
 # Create X and y lists
 #########################
-a__X = numpy.zeros((1, 402)) # There are 402 images of Person6 performing letter A
-a_y = list()
+letter_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y']
 
-b_X = numpy.zeros((1, 370))
-b_y = list()
+for img in img_list:
+    X_list = img_list
+    # Cycle through letters and label images depending on file name
+    for letter in letter_list:
+        path = f"/home/ciara/Documents/ISLDataset/ISL_50k/Frames_{letter}"
+        if fnmatch.fnmatch(img, f"{path}/*"):
+            y_list.append(f"{letter}") 
 
-for img in a_list:
-    a_X = a_list
-    a_y.append("A")
+print(colored("X and y list shapes", 'cyan'))
+print(f"X_list shape: {len(X_list)}, y_list shape: {len(y_list)}\n")
+# print(colored("Sample of X list", "cyan"))
+# print(X_list[1::4000])
+# print(colored(f"\nSample of y list", "cyan"))
+# print(y_list[1::4000])
 
-for image in b_list:
-    b_X = b_list
-    b_y.append("B")
+######################################
+# Split lists into training/testing
+######################################
+X_train,X_test, y_train, y_test = train_test_split(X_list, y_list, train_size=0.8, random_state=42)
 
-print(f"a_X shape: {len(a_X)}, a_y shape: {len(a_y)}, b_X shape: {len(b_X)}, b_y shape: {len(b_y)}")
-
-##########################################
-# Split each list into training/testing
-##########################################
-a_X_train,a_X_test, a_y_train, a_y_test = train_test_split(a_X, a_y)
-
-print(f"a_X_train: {len(a_X_train)}")
-print(f"a_X_test: {len(a_X_test)}")
-print(f"a_y_train: {len(a_y_train)}")
-print(f"a_y_test: {len(a_y_test)}")
-
-b_X_train,b_X_test, b_y_train, b_y_test = train_test_split(b_X, b_y)
-
-print(f"b_X_train: {len(b_X_train)}")
-print(f"b_X_test: {len(b_X_test)}")
-print(f"b_y_train: {len(b_y_train)}")
-print(f"b_y_test: {len(b_y_test)}")
-
-###########################
-# Combine lists together
-###########################
-X_train = a_X_train + b_X_train
-X_test = a_X_test + b_X_test
-y_train = a_y_train + b_y_train
-y_test = a_y_test + b_y_test
-
+print(colored("Training and testing set shapes", 'cyan'))
 print(f"X_train: {len(X_train)}")
 print(f"X_test: {len(X_test)}")
 print(f"y_train: {len(y_train)}")
-print(f"y_test: {len(y_test)}")
-print("")
-print(X_test)
+print(f"y_test: {len(y_test)}\n")
+
+##################
+# Shuffle lists
+##################
+def getShuffleVar():
+    return 0.3
+
+random.shuffle(X_train, getShuffleVar)
+random.shuffle(X_test, getShuffleVar)
+random.shuffle(y_train, getShuffleVar)
+random.shuffle(y_test, getShuffleVar)
+
+################################################
+# Check that lists have been shuffled properly
+#################################################
+# print(coloured("Samples of shuffled X and y test sets", 'cyan'))
+# print(f"{y_test[:10]}\n")
+# print(f"{X_test[:10]}\n")
+
+# print(y_test)
